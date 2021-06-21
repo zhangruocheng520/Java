b784c34b137dc9d5a705c6f196f310410b9c0952
zhangruocheng520 <86142988+zhangruocheng520@users.noreply.github.com>
2021-06-21 08:47:03 +0800

    Add files via upload

---
+++ Zi.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Zi {
+    public static void main(String[] args) {
+        char zifu[]={'a','c','u','b','e','z','f','p'};
+        for (int i=0;i<zifu.length;i++){
+            System.out.print(zifu[i]);
+        }
+        for (int j=0;j<zifu.length-1;j++){
+        }
+        Arrays.sort(zifu);
+        System.out.println("升序排序后");
+        for (char i:zifu){
+
+        }
+    }
+}
